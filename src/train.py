a0333960ea9dd8b440916410683977f1f7ef2d37
z36583425-DR2005 <130303473+syuuu9738@users.noreply.github.com>
2026-05-06 16:09:27 +0000

    ・GPUを用いた学習のためのコードを変更しました。 ・PointNetが、キューブと球の両方を学習できることを確認。

--- src/train.py
+++ src/train.py
@@ -4,24 +4,28 @@ import models.pointnet as PN
 from datasets.dataset import PointNetDataset
 from torch.utils.data import DataLoader
 
-
+#GPUを使うように設定（GPUがない場合はCPUを使用）
+device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
 #データセットの呼び出し
 dataset = PointNetDataset(root_dir="data/train")
 #データローダーの作成
-train_loader = DataLoader(dataset, batch_size=2, shuffle=True)
+train_loader = DataLoader(dataset, batch_size=32, shuffle=True)
 #モデルの呼び出し
-model = PN.PointNet(num_points=1024, num_labels=2)
+model = PN.PointNet(num_points=1024, num_labels=2).to(device)#GPUへ転送
 #損失関数
 criterion = nn.CrossEntropyLoss() 
 #最適化手法
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001) 
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0001) 
 
-num_epochs = 10#10周
-#10個のデータに対して、2個ずつ（バッチ）分けて取り出す
+num_epochs = 50
+#データに対して、バッチ分取り出す
 for epoch in range(num_epochs):
     for i,(data,label) in enumerate(train_loader):
+        # データもGPUに転送し、モデルとデータをGPUで計算できるようにする
+        data = data.to(device)
+        label = label.to(device)
         #データの関係でエラーがでるので、データの形を変える必要がある
-        data = data.view(-1, 3)#1バッチで使うデータを混ぜる
+        data = data.view(-1, 3)#1バッチで使うデータを並べる
         #勾配の初期化
         optimizer.zero_grad()
         #順伝播
@@ -38,5 +42,5 @@ for epoch in range(num_epochs):
 #モデルの保存
 torch.save(model.state_dict(), "pointnet_model.pth")
 print('学習完了！ モデルを保存しました。')
-#1エポックにつき 5回のステップ（更新）」が行われ、
-#それが 10周（エポック）繰り返される ため、合計で 50回 モデルの重みが更新される
+#1エポックにつき n回のステップ（更新）」が行われ、
+#それが m周（エポック）繰り返される ため、合計で nm回 モデルの重みが更新される
